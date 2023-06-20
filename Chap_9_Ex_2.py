61cd0aec42bea5b6c005567c99c9ab2f8857b5b9
SelimTopcu <106619573+SelimTopcu@users.noreply.github.com>
2023-06-20 23:49:29 +0200

    Chapter 9 Excercise 2
    Exercise 2: Write a program that categorizes each mail message by which day of the week the commit was done. To do this look for lines that start with “From”, then look for the third word and keep a running count of each of the days of the week. At the end of the program print out the contents of your dictionary (order does not matter).

---
+++ Chap_9_Ex_2.py
@@ -0,0 +1,33 @@
+#Exercise 2: Write a program that categorizes each mail message by which day of the week the commit was done. To do this look for lines that start with “From”, then look for the third word and keep a running count of each of the days of the week. At the end of the program print out the contents of your dictionary (order does not matter).
+
+#Sample Line:
+#From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+
+#Sample Execution:
+
+#python dow.py
+#Enter a file name: mbox-short.txt
+#{'Fri': 20, 'Thu': 6, 'Sat': 1}
+
+
+fname = input('Enter the file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    exit()
+
+counts = dict()
+for line in fhand:
+    line=line.rstrip()
+    words = line.split()
+        
+    if not line.startswith('From '): continue
+            
+    else:
+        if words[2] not in counts:
+            counts[words[2]] = 1
+        else:
+            counts[words[2]] += 1
+
+print(counts)

0ef880f52ad6a73bb6afb84eef1631ff0ef6f7ea
SelimTopcu <106619573+SelimTopcu@users.noreply.github.com>
2023-06-21 20:13:57 +0200

    Chapter 9 Excercise 5
    Exercise 5: This program records the domain name (instead of the address) where the message was sent from instead of who the mail came from (i.e., the whole email address). At the end of the program, print out the contents of your dictionary.
    
    python schoolcount.py
    Enter a file name: mbox-short.txt
    {'media.berkeley.edu': 4, 'uct.ac.za': 6, 'umich.edu': 7,
    'gmail.com': 1, 'caret.cam.ac.uk': 1, 'iupui.edu': 8}

---
+++ Chap_9_Ex_5.py
@@ -0,0 +1,27 @@
+#Exercise 5: This program records the domain name (instead of the address) where the message was sent from instead of who the mail came from (i.e., the whole email address). At the end of the program, print out the contents of your dictionary.
+
+counts = dict()
+fname = input('Enter the file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    exit()
+
+for line in fhand:
+    line=line.rstrip()
+    words = line.split()
+        
+    if not line.startswith('From '): continue
+            
+    else:
+        atpos = words[1].find('@')       # Position of '@'
+        sppos = words[1].find(' ',atpos)    #Position space after @
+        domain = words[1][atpos+1:]       # Store characters after '@'
+        
+        if domain not in counts:
+            counts[domain] = 1               #First entry
+        else:
+            counts[domain] += 1               # Additional counts
+            
+print(counts)

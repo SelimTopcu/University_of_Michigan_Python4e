82fd1f7464b3d4c4fc225b1dde44e25e10c0c533
SelimTopcu <106619573+SelimTopcu@users.noreply.github.com>
2023-08-01 18:43:04 +0200

    Chapter 11 assignment
    # Finding Numbers in a Haystack
    
    #In this assignment you will read through and parse a file with text and numbers. You will extract all the numbers in the file and compute the sum of the numbers.
    
    #Data Files
    #We provide two files for this assignment. One is a sample file where we give you the sum for your testing and the other is the actual data you need to process for the assignment.
    
    #Sample data: http://py4e-data.dr-chuck.net/regex_sum_42.txt (There are 90 values with a sum=445833)
    #Actual data: http://py4e-data.dr-chuck.net/regex_sum_1829035.txt (There are 68 values and the sum ends with 129)
    #These links open in a new window. Make sure to save the file into the same folder as you will be writing your Python program. #Note: Each student will have a distinct data file for the assignment - so only use your own data file for analysis.

---
+++ chap11_assignment.py
@@ -0,0 +1,30 @@
+# Finding Numbers in a Haystack
+
+#In this assignment you will read through and parse a file with text and numbers. You will extract all the numbers in the file and compute the sum of the numbers.
+
+#Data Files
+#We provide two files for this assignment. One is a sample file where we give you the sum for your testing and the other is the actual data you need to process for the assignment.
+
+#Sample data: http://py4e-data.dr-chuck.net/regex_sum_42.txt (There are 90 values with a sum=445833)
+#Actual data: http://py4e-data.dr-chuck.net/regex_sum_1829035.txt (There are 68 values and the sum ends with 129)
+#These links open in a new window. Make sure to save the file into the same folder as you will be writing your Python program. #Note: Each student will have a distinct data file for the assignment - so only use your own data file for analysis.
+
+import re
+count=0
+total=0
+fhand = open('regex_sum_1829035.txt')
+    
+for line in fhand:
+    line = line.rstrip()
+    x = re.findall('[0-9]+', line)
+    #print(x)
+    if len(x) > 0:
+        for n in x:
+            number=int(n)
+            count=count+1
+            total=total+number
+            print(number)
+        
+
+print("Count:", count)
+print("Total:", total)

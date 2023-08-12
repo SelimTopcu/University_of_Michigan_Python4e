27cbfc159e122df83b7ac65b264aad023386c5f3
SelimTopcu <106619573+SelimTopcu@users.noreply.github.com>
2023-08-12 18:18:19 +0200

    Chapter 12 week 4 assignment 1
    Scraping Numbers from HTML using BeautifulSoup In this assignment you will write a Python program similar to http://www.py4e.com/code3/urllink2.py. The program will use urllib to read the HTML from the data files below, and parse the data, extracting numbers and compute the sum of the numbers in the file.
    
    We provide two files for this assignment. One is a sample file where we give you the sum for your testing and the other is the actual data you need to process for the assignment.
    
    Sample data: http://py4e-data.dr-chuck.net/comments_42.html (Sum=2553)
    Actual data: http://py4e-data.dr-chuck.net/comments_1829037.html (Sum ends with 5)
    You do not need to save these files to your folder since your program will read the data directly from the URL. Note: Each student will have a distinct data url for the assignment - so only use your own data url for analysis.

---
+++ chap_12_assignment_2.py
@@ -0,0 +1,32 @@
+#Scraping Numbers from HTML using BeautifulSoup In this assignment you will write a Python program similar to http://www.py4e.com/code3/urllink2.py. The program will use urllib to read the HTML from the data files below, and parse the data, extracting numbers and compute the sum of the numbers in the file.
+
+#We provide two files for this assignment. One is a sample file where we give you the sum for your testing and the other is the actual data you need to process for the assignment.
+
+#Sample data: http://py4e-data.dr-chuck.net/comments_42.html (Sum=2553)
+#Actual data: http://py4e-data.dr-chuck.net/comments_1829037.html (Sum ends with 5)
+#You do not need to save these files to your folder since your program will read the data directly from the URL. Note: Each student will have a distinct data url for the assignment - so only use your own data url for analysis.
+#Data Format
+#The file is a table of names and comment counts. You can ignore most of the data in the file except for lines like the following:
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+lst=list()
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+html = urlopen('http://py4e-data.dr-chuck.net/comments_1829037.html', context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+# Retrieve all of the span tags
+tags = soup('span')
+for tag in tags:
+    #print('TAG:', tag)
+    #print('Contents:', tag.contents[0])
+    lst.append(int(tag.contents[0]))
+
+print(sum(lst))
+    

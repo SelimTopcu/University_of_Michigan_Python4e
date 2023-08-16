c07508f3c202f8353ec3ce846dcc1124235a3d8a
SelimTopcu <106619573+SelimTopcu@users.noreply.github.com>
2023-08-16 20:15:26 +0200

    Chapter 13, Week 6 assignment 1
    Extracting Data from JSON
    
    In this assignment you will write a Python program somewhat similar to http://www.py4e.com/code3/json2.py. The program will prompt for a URL, read the JSON data from that URL using urllib and then parse and extract the comment counts from the JSON data, compute the sum of the numbers in the file and enter the sum below:
    We provide two files for this assignment. One is a sample file where we give you the sum for your testing and the other is the actual data you need to process for the assignment.
    
    Sample data: http://py4e-data.dr-chuck.net/comments_42.json (Sum=2553)
    Actual data: http://py4e-data.dr-chuck.net/comments_1829040.json (Sum ends with 74)
    You do not need to save these files to your folder since your program will read the data directly from the URL. Note: Each student will have a distinct data url for the assignment - so only use your own data url for analysis.
    Data Format
    The data consists of a number of names and comment counts in JSON as follows:

---
+++ chap_13_assignment2.py
@@ -0,0 +1,33 @@
+#Extracting Data from JSON
+
+#In this assignment you will write a Python program somewhat similar to http://www.py4e.com/code3/json2.py. The program will prompt for a URL, read the JSON data from that URL using urllib and then parse and extract the comment counts from the JSON data, compute the sum of the numbers in the file and enter the sum below:
+#We provide two files for this assignment. One is a sample file where we give you the sum for your testing and the other is the actual data you need to process for the assignment.
+
+#Sample data: http://py4e-data.dr-chuck.net/comments_42.json (Sum=2553)
+#Actual data: http://py4e-data.dr-chuck.net/comments_1829040.json (Sum ends with 74)
+#You do not need to save these files to your folder since your program will read the data directly from the URL. Note: Each student will have a distinct data url for the assignment - so only use your own data url for analysis.
+#Data Format
+#The data consists of a number of names and comment counts in JSON as follows:
+
+import urllib.request, urllib.parse, urllib.error
+import json
+
+
+lst=list()
+number=0
+url = input('Enter URL: ')
+uh = urllib.request.urlopen(url)
+data = uh.read()
+print('Retrieved', len(data), 'characters')
+
+info = json.loads(data)
+
+for item in info["comments"]:
+    number=number+1
+    lst.append(int(item['count']))
+    #print(item['count'])
+    
+print(number)            
+print(sum(lst))
+
+
